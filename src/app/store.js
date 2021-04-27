923bfc1af5773d8579d5ba873242e62b78e7bd1c
latoyaallen <me@tailorlabs.io>
2021-04-27 14:35:00 -0400

    Import conversionReducer into store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import conversionReducer from '../features/conversion/conversionSlice'
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    conversion: conversionReducer,
+  },
 })
